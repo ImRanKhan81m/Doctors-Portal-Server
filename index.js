b82c097c97dbb2a29fd42e93f7bc10948300fdc0
Imran Hossen <freelancerimran81@gmail.com>
2022-05-18 14:35:37 +0600

    done

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
+const { ObjectID } = require('bson');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -88,6 +89,21 @@ async function run() {
     // =====================================================================>
     // =====================================================================>
 
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    })
+
+    // =====================================================================>
+    // =====================================================================>
+
     app.get('/service', async (req, res) => {
       const query = {};
       const cursor = servicesCollection.find(query).project({ name: 1 });
@@ -144,6 +160,16 @@ async function run() {
         return res.status(403).send({ message: 'Forbidden Access' })
       }
 
+    })
+
+    // =====================================================================>
+    // =====================================================================>
+
+    app.get('/booking/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectID(id) };
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking)
     })
 
     // =====================================================================>
